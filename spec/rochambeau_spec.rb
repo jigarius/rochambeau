ad6d5112b9c1ccc8d8757af8bb559968d3427e0e
Jigar Mehta <hello@jigarius.com>
2021-02-24 10:40:19 -0500

    Test: class Rochambeau

--- spec/rochambeau_spec.rb
+++ spec/rochambeau_spec.rb
@@ -1,8 +1,6 @@
 # typed: false
 # frozen_string_literal: true
 
-require_relative '../lib/rochambeau'
-
 describe Rochambeau do
   it '::VERSION' do
     expect(Rochambeau::VERSION).to match(/^\d+\.\d\.\d+$/)
@@ -14,5 +12,32 @@ describe Rochambeau do
     )
 
     expect(Rochambeau::VERSION).to eq(gemspec.version.to_s)
+  end
+
+  it '.execute calls Cli.start(ARGV)' do
+    expect(Rochambeau::Cli).to receive(:start).with(ARGV).and_return(true)
+
+    Rochambeau.execute
+  end
+
+  context 'on interrupt' do
+    it '.execute exists gracefully' do
+      expect(Rochambeau::Cli)
+        .to receive(:start)
+        .and_raise(Interrupt)
+
+      expect { Rochambeau.execute }.to output('').to_stdout
+    end
+  end
+
+  context 'on argument error' do
+    it '.execute exists gracefully' do
+      expect(Rochambeau::Cli)
+        .to receive(:start)
+        .and_raise(ArgumentError, 'Illegal argument --foobar')
+
+      expect { Rochambeau.execute }
+        .to output("Illegal argument --foobar\n").to_stderr
+    end
   end
 end
