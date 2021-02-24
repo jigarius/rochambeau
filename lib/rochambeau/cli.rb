aed2ed049d66623b04816a5d2764b93b49685261
Jigar Mehta <hello@jigarius.com>
2021-02-24 11:55:11 -0500

    Cleanup and improve testability

--- lib/rochambeau/cli.rb
+++ lib/rochambeau/cli.rb
@@ -32,39 +32,43 @@ module Rochambeau
 
       game_mode =
         options.advanced ? GameMode::ADVANCED : GameMode::BASIC
-
-      if options.players == 1
-        p1 = Player::UnnamedHuman.new
-        p2 = Player::Robot.new
-      else
-        p1 = Player::NamedHuman.new('Player 1')
-        p2 = Player::NamedHuman.new('Player 2')
-      end
-
-      T.let(p1, Player)
-      T.let(p2, Player)
+      p1, p2 = Cli.get_players(options.players)
 
       puts game_mode.options.map(&:label).join(' · ')
       p1_choice = p1.choose_option(game_mode.options)
       p2_choice = p2.choose_option(game_mode.options)
 
-      puts '------'
-      puts "#{p1.name}: #{p1_choice}"
-      puts "#{p2.name}: #{p2_choice}"
-      puts '------'
+      puts <<~CHOICES
+        ------
+        #{p1.name}: #{p1_choice}
+        #{p2.name}: #{p2_choice}
+        ------
+      CHOICES
 
       puts Option.explain(p1_choice, p2_choice) unless p1_choice == p2_choice
 
       case p1_choice <=> p2_choice
-      when 1
-        puts p1.victory_message
-      when -1
-        puts p2.victory_message
-      else
-        puts 'Match draw.'
+      when 1 then puts p1.victory_message
+      when -1 then puts p2.victory_message
+      else puts 'Match draw.'
       end
     end
 
     default_task :play
+
+    sig { params(count: Integer).returns([Player, Player]) }
+    def self.get_players(count)
+      case count
+      when 1
+        return [Player::UnnamedHuman.new, Player::Robot.new]
+      when 2
+        return [
+          Player::NamedHuman.new('Player 1'),
+          Player::NamedHuman.new('Player 2'),
+        ]
+      end
+
+      raise ArgumentError, "Unexpected player count: #{count}"
+    end
   end
 end
